e6feef4852ab7208ee9af4f2ffe605d15edf2380
Nathan Cale Wichman <35352401+NateWichman@users.noreply.github.com>
2018-11-01 21:35:31 -0400

    Added Notes

--- ClientLab3.java
+++ ClientLab3.java
@@ -1,7 +1,15 @@
 import java.io.*;
 import java.net.Socket;
 import java.util.Scanner;
+/***********************************************************************************************
+Runs the client side of the simple instant messenging software. Allows for continous converation
+between the server and the client through threads. This class only uses one thread instead of 
+two because using two is kind of redundant in the other one. The server uses two because I want
+to modify it later to handle multiple clients at once.
 
+@Author Nathan Wichman
+@Version Fall 2018
+***********************************************************************************************/
 public class ClientLab3{
 
     public static void main(String args[]) throws IOException{
@@ -22,19 +30,26 @@ public class ClientLab3{
 	    Socket socket = null;
 	    PrintWriter out = null;
 	    BufferedReader in = null;
+	    //Setting up the Servers socket
 	    socket = new Socket(serverHostname, portNumber);
+	    //For sending informtaiont
 	    out = new PrintWriter(socket.getOutputStream(), true);
+	    //For receiving information
 	    in = new BufferedReader(new InputStreamReader(
 							  socket.getInputStream()));
-
+	    //For typing information
 	    BufferedReader stdIn = new BufferedReader(
 						      new InputStreamReader(System.in));
-	     String userInput;
-
+	    //This string holds the last typed message.
+	    String userInput;
+		
+            //Starts a new thread to receive information.
 	    new receiveThread2(socket).start();
 
+	     //This block is essentailly the other thread, sends information
 	    System.out.println("Input: ");
 	    while ((userInput = stdIn.readLine()) != null) {
+		//Sending information out the print stream
 		out.println(userInput);
 		System.out.print ("Input: ");
 		if(userInput.equals("Exit")){
